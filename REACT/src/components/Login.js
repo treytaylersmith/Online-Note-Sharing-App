a2368fdbf32f119955584b531def1d74c94f270d
bebo1719 <142172190+bebo1719@users.noreply.github.com>
2024-11-07 18:43:15 -0600

    react

---
+++ REACT/src/components/Login.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import api from '../api';
+
+const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const { data } = await api.post('/auth/login', { email, password });
+        localStorage.setItem('token', data.token);
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+            <button type="submit">Login</button>
+        </form>
+    );
+};
+
+export default Login;
+
