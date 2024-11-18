ec7efb146d84154d1b8e6b17ef8d2af91dce8609
Trey Tayler Smith <treytaylersmith@gmail.com>
2024-11-18 00:39:56 -0600

    server.js updated

--- server/server.js
+++ server/server.js
@@ -34,7 +34,15 @@ const startApolloServer = async () => {
   app.use('/graphql', expressMiddleware(server, {
     context: authMiddleware
   }));
-
+  
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+    
+    app.get('/', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  }
+  
   app.post('/api/chat', async (req, res) => {
     const { messages } = req.body; // Expecting chat history
     try {
@@ -59,14 +67,7 @@ const startApolloServer = async () => {
     }
   });
 
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
-    
-    app.get('/', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-    });
-  }
-  
+
 
   db.once('open', () => {
     app.listen(port, () => {
